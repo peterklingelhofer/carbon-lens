e0fb14bc0530156df9c94dd9a8b9169e94d1134e
Peter Klingelhofer <peterklingelhofer@gmail.com>
2026-09-07 17:45:00 -0400

    style(web): use Ubuntu Mono for code and identifiers

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -7,6 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import "@fontsource/ubuntu/400.css";
 import "@fontsource/ubuntu/500.css";
 import "@fontsource/ubuntu/700.css";
+// Ubuntu Mono ships 400 and 700; StatCard renders identifiers at 700.
+import "@fontsource/ubuntu-mono/400.css";
+import "@fontsource/ubuntu-mono/700.css";
 import App from "./App";
 import "./index.css";
 
